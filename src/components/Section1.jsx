3ab10fa0cee44f5f85f0f6d2afc2066382d961fd
Yotman Reyes <yotman.reyes@gmail.com>
2023-06-23 16:53:52 -0400

    feat:add spotify button

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -4,6 +4,10 @@ export default function Section1() {
         <div className='image-1'></div>
         <h1>¿Quieres crear tu propio podcast?</h1>
         <p>¡Tenemos la solución perfecta para ti! Ofrecemos un servicio de creación de podcast desde cero, en el que te guiaremos a través de cada paso del proceso.</p>
+        <button type="button" className="auth-button" id="spotifyLoginButton">
+          Login with Spotify
+          <svg className="auth-button__icon" xmlns="http://www.w3.org/2000/svg" fill="#000000" height="150" preserveAspectRatio="xMidYMid meet" version="1" viewBox="0.0 0.0 150.0 150.0" width="150" zoomAndPan="magnify"><g><g><g><g id="change1_1"><path d="M75,0C33.8,0,0,33.8,0,75s33.8,75,75,75s75-33.8,75-75S116.6,0,75,0z M109.5,108.4 c-1.5,2.2-4.1,3-6.4,1.5c-17.6-10.9-39.7-13.1-66-7.1c-2.6,0.8-4.9-1.1-5.6-3.4c-0.8-2.6,1.1-4.9,3.4-5.6 c28.5-6.4,53.3-3.8,72.8,8.2C110.3,103.1,110.6,106.1,109.5,108.4z M118.5,87.7c-1.9,2.6-5.3,3.8-7.9,1.9 C90.4,77.2,59.6,73.5,36,81c-3,0.8-6.4-0.8-7.1-3.8c-0.8-3,0.8-6.4,3.8-7.1C60,61.9,93.8,66,117,80.2 C119.3,81.4,120.4,85.1,118.5,87.7z M119.3,66.8c-24-14.2-64.1-15.8-87-8.6c-3.8,1.1-7.5-1.1-8.6-4.5c-1.1-3.8,1.1-7.5,4.5-8.6 c26.6-7.9,70.5-6.4,98.2,10.1c3.4,1.9,4.5,6.4,2.6,9.7C127.1,67.5,122.6,68.6,119.3,66.8z" fill="#ffffff"/></g></g></g></g></svg>
+        </button>
     </section>
   )
 }
